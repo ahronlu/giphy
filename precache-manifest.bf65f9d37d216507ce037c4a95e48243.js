4feab8fa6c5ad09f5dc9bb4ffb78f1988cf24206
koftov <ahronlu@gmail.com>
2020-07-30 03:39:06 +0300

    Updates

---
+++ precache-manifest.bf65f9d37d216507ce037c4a95e48243.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3cdab270d24df933fbf94b2e0b0d8e9",
+    "url": "/giphy/index.html"
+  },
+  {
+    "revision": "5eba7a2f5f7e0741ddac",
+    "url": "/giphy/static/css/main.f8e75c11.chunk.css"
+  },
+  {
+    "revision": "0d15d5bd0fe752b947cc",
+    "url": "/giphy/static/js/2.4a10166c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/giphy/static/js/2.4a10166c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eba7a2f5f7e0741ddac",
+    "url": "/giphy/static/js/main.e30d4815.chunk.js"
+  },
+  {
+    "revision": "b40e66dd66b453311767",
+    "url": "/giphy/static/js/runtime-main.fae907ec.js"
+  }
+]);
